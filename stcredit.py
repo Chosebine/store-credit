67bbc5ddf7f28f127b5ca8ab1241e070676dca9f
pi <math.dumas@gmail.com>
2016-10-14 10:54:28 -0400

    reussi avec methode brute force

--- stcredit.py
+++ stcredit.py
@@ -1,8 +1,19 @@
-# input() reads a string with a line of input, stripping the '\n' (newline) at the end.
-# This is all you need for most Google Code Jam problems.
-t = int(input())  # read a line with a single integer
-for i in range(1, t + 1):
-    credit = int(input())
-    nbitem = int(input())
-    prix = [int(s) for s in input().split(" ")]
-    print(credit,nbitem,prix)
+
+def solve(cr, nbitem, itemprice):
+    for b in range(nbitem):
+        for c in range(b+1,nbitem):
+            if itemprice[b]+itemprice[c] == cr:
+                return[b+1,c+1]
+
+
+
+f = open('input.txt', 'r')
+ncase = int(f.readline())
+
+for a in range(1,ncase+1):
+    cCredit = int(f.readline())
+    iItem = int(f.readline())
+    pPrice = list(map(int, (str.split(f.readline(),' '))))
+    solution = solve(cCredit, iItem, pPrice)
+    print('Case #'+str(a)+':', solution[0],solution[1])
+f.close()
