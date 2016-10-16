f00c9f931ff9b26895bbd55a12b80094e4f35d55
pi <math.dumas@gmail.com>
2016-10-16 09:24:11 -0400

    reussi avec methode brute force

--- stcredit.py
+++ stcredit.py
@@ -6,6 +6,17 @@ def bruteforce(cr, nbitem, itemprice):
                 return[b+1,c+1]
 
 
+def clever(cr,nbitem,itemprice):
+    sol = {}
+    for d in range(nbitem):
+        dif = cr - itemprice[d]
+        if dif not in sol:
+            sol[itemprice[d]] = d
+        else:
+            return [sol[dif]+1,d+1]
+    return sol
+
+
 
 f = open('input.txt', 'r')
 ncase = int(f.readline())
@@ -14,6 +25,6 @@ for a in range(1,ncase+1):
     cCredit = int(f.readline())
     iItem = int(f.readline())
     pPrice = list(map(int, (str.split(f.readline(),' '))))
-    solution = bruteforce(cCredit, iItem, pPrice)
-    print('Case #'+str(a)+':', solution[0],solution[1])
+    solution = clever(cCredit, iItem, pPrice)
+    print("Case #"+str(a)+":",solution[0],solution[1])
 f.close()
